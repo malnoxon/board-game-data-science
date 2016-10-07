89b9c8dde22a1ea5f6830e3ea6dc1ad356c42b43
Daniel Daphron Kaczmarek <xenodaphron@gmail.com>
2016-10-07 16:40:07 -0500

    writes output csv for sample files

--- boardgameprices/parsing.py
+++ boardgameprices/parsing.py
@@ -1,25 +1,44 @@
 from bs4 import BeautifulSoup
 import re
+import csv
+import tempfile
 
-name = []
-players = []
-play_time = []
-age = []
-published = []
-bgg_link = []
 
-def scrape(filename):
+def to_csv(csv_writer, name, players, play_time, age, published, bgg_link, store_names, price, free_shipping, links, status, international):
+    with tempfile.SpooledTemporaryFile(max_size=10000, mode='w+') as tmp_file:
+        tmp_writer = csv.writer(tmp_file)
+        tmp_writer.writerow(store_names)
+        tmp_writer.writerow(price)
+        tmp_writer.writerow(free_shipping)
+        tmp_writer.writerow(links)
+        tmp_writer.writerow(status)
+        tmp_writer.writerow(international)
+        tmp_file.seek(0)
+        store_names_joined = tmp_file.readline().strip("\n")
+        price_joined = tmp_file.readline().strip("\n")
+        free_shipping_joined = tmp_file.readline().strip("\n")
+        links_joined = tmp_file.readline().strip("\n")
+        status_joined = tmp_file.readline().strip("\n")
+        international_joined = tmp_file.readline().strip("\n")
+
+        for i in range(len(store_names)):
+            print(store_names[i])
+
+        csv_writer.writerow([name, players, play_time, age, published, bgg_link, store_names_joined, price_joined, free_shipping_joined, links_joined, status_joined, international_joined])
+
+
+def scrape(filename, csv_writer):
     soup = BeautifulSoup(open(filename), "html.parser")
     title = soup.find("div", {"class": "main-title"})
-    name.append(title.text)
+    name = title.text
 
     metabar = soup.findAll("div", {"class" : "meta-bar"})
     stats = metabar[0].findAll("strong")
     stats = [x.text for x in stats]
-    players.append(stats[0])
-    play_time.append(stats[1])
-    age.append(stats[2])
-    published.append(stats[3])
+    players = stats[0]
+    play_time = stats[1]
+    age = stats[2]
+    published = stats[3]
 
     for elem in soup(text=re.compile('Game info on BoardGameGeek.com')):
         bgg_link = elem.parent['href']
@@ -60,7 +79,7 @@ def scrape(filename):
             price.append(price_elem.text)
             free_shipping_elem = price_elem.next_sibling
             if free_shipping_elem.span:
-                free_shipping.append(free_shipping_elem.span['data-original-title'])
+                free_shipping.append(BeautifulSoup(free_shipping_elem.span['data-original-title']).b.text)
             in_stock_elem = free_shipping_elem.next_sibling
             status.append(in_stock_elem.span.text)
             links.append(elem['href'])
@@ -77,7 +96,7 @@ def scrape(filename):
             price.append(price_elem.text)
             free_shipping_elem = price_elem.next_sibling
             if free_shipping_elem.span:
-                free_shipping.append(free_shipping_elem.span['data-original-title'])
+                free_shipping.append(BeautifulSoup(free_shipping_elem.span['data-original-title']).b.text)
             in_stock_elem = free_shipping_elem.next_sibling
             status.append(in_stock_elem.span.text)
             links.append(elem['href'])
@@ -91,11 +110,15 @@ def scrape(filename):
     print(status)
     print(international)
 
+    to_csv(csv_writer, name, players, play_time, age, published, bgg_link, store_names, price, free_shipping, links, status, international)
+
 
 
 if __name__ == "__main__":
-    scrape("codename.html")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    # scrape("sushigoparty.html")
+    with open('test.out', 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        scrape("codename.html", csv_writer)
+        print("------------------------------------------------------------")
+        print("------------------------------------------------------------")
+        print("------------------------------------------------------------")
+        scrape("sushigoparty.html", csv_writer)
