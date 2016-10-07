24298bfe741485263efb0f4d23eb5517a7fe9f3d
Daniel Daphron Kaczmarek <xenodaphron@gmail.com>
2016-10-07 15:08:45 -0500

    Done with actual parsing of the US and international lists

--- boardgameprices/parsing.py
+++ boardgameprices/parsing.py
@@ -31,6 +31,7 @@ def scrape(filename):
     country = []
     links = []
     free_shipping = []
+    international = []
 
     # <div id="price-tables"> is US Stores
         # <div data-prices="media-sponsors">
@@ -43,6 +44,12 @@ def scrape(filename):
         # ...
     # price_table = soup.findAll("div", {"id" : "price-tables"})
     price_tables = soup.findAll("div", {"id" : "price-tables"})
+    ebay_table = soup.findAll("div", {"class": "ebay-results"})
+    international_table = ebay_table[0].next_sibling.next_sibling
+    print(international_table)
+    print("US TABLE")
+    print("**********************************")
+    print("**********************************")
     for table in price_tables:
         table = table.div # avoid the stupid duplicate (class visible-xs shadowed-listing)
         for elem in table.findAll(['a']):
@@ -57,12 +64,32 @@ def scrape(filename):
             in_stock_elem = free_shipping_elem.next_sibling
             status.append(in_stock_elem.span.text)
             links.append(elem['href'])
+            international.append("False")
+
+    # International table
+    print("INTERNATIONAL TABLE")
+    for table in [international_table]:
+        for elem in table.findAll(['a']):
+            if elem.text == "Visit Store":
+                continue
+            store_names.append(elem.text)
+            price_elem = elem.parent.parent.next_sibling
+            price.append(price_elem.text)
+            free_shipping_elem = price_elem.next_sibling
+            if free_shipping_elem.span:
+                free_shipping.append(free_shipping_elem.span['data-original-title'])
+            in_stock_elem = free_shipping_elem.next_sibling
+            status.append(in_stock_elem.span.text)
+            links.append(elem['href'])
+            international.append("True")
+
 
     print(store_names)
     print(price)
     print(free_shipping)
     print(links)
     print(status)
+    print(international)
 
 
 
