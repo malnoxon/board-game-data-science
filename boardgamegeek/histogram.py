95397757b0dcca71e0825651fe59cf2e7a126b83
Daniel Daphron Kaczmarek <xenodaphron@gmail.com>
2016-11-08 16:21:16 -0600

    updated Untitled.ipynb to be cleaner for submission

--- boardgamegeek/histogram.py
+++ boardgamegeek/histogram.py
@@ -6,18 +6,18 @@ with open('newTableA.csv', 'r') as csv_in:
     csv_reader = csv.DictReader(csv_in)
 
     first_row = True
-    min_ages = []
+    min_playerss = []
     for row in csv_reader:
-        if row['min_age'] != '':
-            min_ages.append(float(row['min_age']))
-            if float(row['min_age']) < 2:
+        if row['min_players'] != '':
+            min_playerss.append(float(row['min_players']))
+            if float(row['min_players']) > 5:
                 # print(row)
                 print(row['name'])
-                print(row['min_age'])
+                print(row['min_players'])
 
 
     # gaussian_numbers = np.random.randn(1000)
-    hist= plt.hist(np.asarray(min_ages).astype(np.int), bins=range(40), range=(0,40))
+    hist= plt.hist(np.asarray(min_playerss).astype(np.int), bins=range(40), range=(0,40))
     plt.title("Minimum Age Histogram")
     plt.xlabel("Minimum Age")
     plt.ylabel("Frequency")
@@ -26,5 +26,5 @@ with open('newTableA.csv', 'r') as csv_in:
     print(hist)
     print(bin_edges)
 
-    # print(max(min_ages))
+    # print(max(min_playerss))
 
